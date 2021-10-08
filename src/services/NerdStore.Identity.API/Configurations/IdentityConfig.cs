39473da66f327bd53ba55a5ccfd81444c9020804
Fernando Henrique Gonçalves Marreca <fernando.dextra@dotzmkt.com.br>
2021-10-08 08:47:05 -0300

    Project structure

---
+++ src/services/NerdStore.Identity.API/Configurations/IdentityConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NerdStore.Identity.API.Data;
+
+namespace NerdStore.Identity.API.Configurations
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("NerdStoreConnection"));
+            });
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
